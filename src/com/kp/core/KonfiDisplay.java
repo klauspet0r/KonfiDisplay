a8733ef1bd2e78d1ac1a148642e1682dcb66f829
klauspeter <klauspeter@KLAUSPETER>
2017-07-03 21:37:58 +0200

    added deps and few basic lines

--- src/com/kp/core/KonfiDisplay.java
+++ src/com/kp/core/KonfiDisplay.java
@@ -1,11 +1,48 @@
 package com.kp.core;
 
+import microsoft.exchange.webservices.data.autodiscover.IAutodiscoverRedirectionUrl;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+
 public class KonfiDisplay {
 
 	public static void main(String[] args) 
 	{
-		// TODO Add some!
+		ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+		ExchangeCredentials credentials = new WebCredentials("email@d.dy", "password");
+		service.setCredentials(credentials);
+		
+		try 
+		{
+			service.autodiscoverUrl("email@d.dy");
+		} 
+		catch (Exception e) 
+		{			
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			service.autodiscoverUrl("email@d.dy", new RedirectionUrlCallback());
+		} 
+		catch (Exception e) 
+		{			
+			e.printStackTrace();
+		}
+		
+		
+		
+		service.close();
 
 	}
+	
+	static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {
+        public boolean autodiscoverRedirectionUrlValidationCallback(
+                String redirectionUrl) {
+            return redirectionUrl.toLowerCase().startsWith("https://");
+        }
+    }
 
 }
